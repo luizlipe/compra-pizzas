f1aa9e87ecc7ed0615c21a7b256885cebdcea77a
luizlipe <felipe250718@gmail.com>
2024-12-18 09:13:47 -0300

    feat: adicionado função para listar as pizzas

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const cs = (el)=>document.querySelectorAll(el)
 
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true)
-    //Preencher as informações em pizzaItem
+
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    
 
     c('.pizza-area').append( pizzaItem )
 })
