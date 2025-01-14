e23fb279fd88c649f00544f18d0ae53d4b39f7f7
luizlipe <felipe250718@gmail.com>
2025-01-14 10:24:00 -0300

    feat: listando pizzas no carrinho

--- script.js
+++ script.js
@@ -80,11 +80,8 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
 })
 c('.pizzaInfo--addButton').addEventListener('click', () => {
     let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'))
-
     let identifier = pizzaJson[modalKey].id+'@'+size
-
     let key = cart.findIndex((item) => item.identifier == identifier)
-
     if(key > -1) {
         cart[key].qt += modalQt
     }else {
@@ -96,6 +93,39 @@ c('.pizzaInfo--addButton').addEventListener('click', () => {
 
     })
 }
+    updateCart()
     closeModal()
     
 })
+
+function updateCart() {
+    if(cart.length > 0){
+        c('aside').classList.add('show')
+        c('.cart').innerHTML = ''
+        for(let i in cart){
+          let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id) 
+          let cartItem = c('.models .cart--item').cloneNode(true)
+          let pizzaSizeName
+          switch(cart[i].size){
+            case 0:
+                pizzaSizeName = 'P'
+                break
+            case 1:
+                pizzaSizeName = 'M'
+                break
+            case 2:
+                pizzaSizeName = 'G'
+                break            
+          }
+          let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`
+
+          cartItem.querySelector('img').scr = pizzaItem.img
+          cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName
+          cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt
+
+          c('.cart').append(cartItem)
+        }
+    }else{
+        c('aside').classList.remove('show')
+    }
+}
